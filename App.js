419ce78ecba8d586b8063eeccf58c14ee9f1e99b
OneDodge <71073232+OneDodge@users.noreply.github.com>
2020-09-17 22:43:22 +0800

    update

--- App.js
+++ App.js
@@ -7,9 +7,35 @@ import HomeScreen from './components/HomeScreen';
 
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
+import { ThemeProvider, TabBar } from 'react-native-ios-kit';
+
+import { PageControlView } from 'react-native-ios-kit';
 
 export default function App() {
   return (
-    <HomeScreen />
+    <TabBar
+  tabs={[
+    {
+      icon: 'ios-paper-outline',
+      title: 'WOWch'
+    },
+    {
+      icon: 'ios-podium',
+      title: 'Alerts'
+    },
+    {
+      icon: 'ios-star-outline',
+      title: 'Portfolio'
+    },
+    {
+      icon: 'ios-people',
+      title: 'Products'
+    },
+    {
+      icon: 'ios-people',
+      title: 'Friends'
+    }
+  ]}
+/>
   );
 }
