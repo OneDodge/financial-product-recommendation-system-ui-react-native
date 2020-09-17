3968a7577e4ad980a7b997483c0a5927d4637e37
OneDodge <71073232+OneDodge@users.noreply.github.com>
2020-09-17 23:42:38 +0800

    update

--- App.js
+++ App.js
@@ -1,41 +1,258 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView } from 'react-native';
 import Constants from 'expo-constants';
 
 // You can import from local files
 import HomeScreen from './components/HomeScreen';
+import SearchScreen from './components/SearchScreen'
 
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
-import { ThemeProvider, TabBar } from 'react-native-ios-kit';
+import { ThemeProvider, TabBar, PageControlView, DefaultTheme, Headline } from 'react-native-ios-kit';
+import color from 'color';
 
-import { PageControlView } from 'react-native-ios-kit';
+const theme = {
+  ...DefaultTheme,
+  primaryColor: 'tomato',
+  primaryLightColor: color('tomato')
+    .lighten(0.2)
+    .rgb()
+    .string(),
+  disabledColor: 'yellow',
+};
 
 export default function App() {
-  return (
-    <TabBar
-  tabs={[
-    {
-      icon: 'ios-paper-outline',
-      title: 'WOWch'
-    },
-    {
-      icon: 'ios-podium',
-      title: 'Alerts'
-    },
-    {
-      icon: 'ios-star-outline',
-      title: 'Portfolio'
-    },
-    {
-      icon: 'ios-people',
-      title: 'Products'
-    },
-    {
-      icon: 'ios-people',
-      title: 'Friends'
-    }
-  ]}
-/>
-  );
+  const [activeTab, setActiveTab] = React.useState(0);
+  if (activeTab === 0) {
+    return (
+      <ThemeProvider theme={theme}>
+        <SafeAreaView style={styles.safeView}>
+          <View style={styles.screen}>
+            <HomeScreen />
+            <TabBar tabs={
+              [
+                {
+                  icon: 'ios-paper-outline',
+                  title: 'WOWch',
+                  onPress: () => setActiveTab(0),
+                  isActive: activeTab === 0
+                },
+                {
+                  icon: 'ios-podium',
+                  title: 'Alerts',
+                  onPress: () => setActiveTab(1),
+                  isActive: activeTab === 1
+                },
+                {
+                  icon: 'ios-podium',
+                  title: 'Portfolio',
+                  onPress: () => setActiveTab(2),
+                  isActive: activeTab === 2
+                },
+                {
+                  icon: 'ios-people',
+                  title: 'Products',
+                  onPress: () => setActiveTab(3),
+                  isActive: activeTab === 3
+                },
+                {
+                  icon: 'ios-people',
+                  title: 'Friends',
+                  onPress: () => setActiveTab(4),
+                  isActive: activeTab === 4
+                }
+              ]}
+            />
+          </View>
+        </SafeAreaView>
+      </ThemeProvider>
+    );
+  } else if (activeTab === 1) {
+    return (
+      <ThemeProvider theme={theme}>
+        <SafeAreaView style={styles.safeView}>
+          <View style={styles.screen}>
+            <Headline>Alerts Tab</Headline>
+            <TabBar tabs={
+              [
+                {
+                  icon: 'ios-paper-outline',
+                  title: 'WOWch',
+                  onPress: () => setActiveTab(0),
+                  isActive: activeTab === 0
+                },
+                {
+                  icon: 'ios-podium',
+                  title: 'Alerts',
+                  onPress: () => setActiveTab(1),
+                  isActive: activeTab === 1
+                },
+                {
+                  icon: 'ios-podium',
+                  title: 'Portfolio',
+                  onPress: () => setActiveTab(2),
+                  isActive: activeTab === 2
+                },
+                {
+                  icon: 'ios-people',
+                  title: 'Products',
+                  onPress: () => setActiveTab(3),
+                  isActive: activeTab === 3
+                },
+                {
+                  icon: 'ios-people',
+                  title: 'Friends',
+                  onPress: () => setActiveTab(4),
+                  isActive: activeTab === 4
+                }
+              ]}
+            />
+          </View>
+        </SafeAreaView>
+      </ThemeProvider>
+    );
+  } else if (activeTab === 2) {
+    return (
+      <ThemeProvider theme={theme}>
+        <SafeAreaView style={styles.safeView}>
+          <View style={styles.screen}>
+            <Headline>Portfolio Tab</Headline>
+            <TabBar tabs={
+              [
+                {
+                  icon: 'ios-paper-outline',
+                  title: 'WOWch',
+                  onPress: () => setActiveTab(0),
+                  isActive: activeTab === 0
+                },
+                {
+                  icon: 'ios-podium',
+                  title: 'Alerts',
+                  onPress: () => setActiveTab(1),
+                  isActive: activeTab === 1
+                },
+                {
+                  icon: 'ios-podium',
+                  title: 'Portfolio',
+                  onPress: () => setActiveTab(2),
+                  isActive: activeTab === 2
+                },
+                {
+                  icon: 'ios-people',
+                  title: 'Products',
+                  onPress: () => setActiveTab(3),
+                  isActive: activeTab === 3
+                },
+                {
+                  icon: 'ios-people',
+                  title: 'Friends',
+                  onPress: () => setActiveTab(4),
+                  isActive: activeTab === 4
+                }
+              ]}
+            />
+          </View>
+        </SafeAreaView>
+      </ThemeProvider>
+    );
+  } else if (activeTab === 3) {
+    return (
+      <ThemeProvider theme={theme}>
+        <SafeAreaView style={styles.safeView}>
+          <View style={styles.screen}>
+            <SearchScreen />
+            <TabBar tabs={
+              [
+                {
+                  icon: 'ios-paper-outline',
+                  title: 'WOWch',
+                  onPress: () => setActiveTab(0),
+                  isActive: activeTab === 0
+                },
+                {
+                  icon: 'ios-podium',
+                  title: 'Alerts',
+                  onPress: () => setActiveTab(1),
+                  isActive: activeTab === 1
+                },
+                {
+                  icon: 'ios-podium',
+                  title: 'Portfolio',
+                  onPress: () => setActiveTab(2),
+                  isActive: activeTab === 2
+                },
+                {
+                  icon: 'ios-people',
+                  title: 'Products',
+                  onPress: () => setActiveTab(3),
+                  isActive: activeTab === 3
+                },
+                {
+                  icon: 'ios-people',
+                  title: 'Friends',
+                  onPress: () => setActiveTab(4),
+                  isActive: activeTab === 4
+                }
+              ]}
+            />
+          </View>
+        </SafeAreaView>
+      </ThemeProvider>
+    );
+  } else if (activeTab === 4) {
+    return (
+      <ThemeProvider theme={theme}>
+        <SafeAreaView style={styles.safeView}>
+          <View style={styles.screen}>
+            <Headline>Friends Tab</Headline>
+            <TabBar tabs={
+              [
+                {
+                  icon: 'ios-paper-outline',
+                  title: 'WOWch',
+                  onPress: () => setActiveTab(0),
+                  isActive: activeTab === 0
+                },
+                {
+                  icon: 'ios-podium',
+                  title: 'Alerts',
+                  onPress: () => setActiveTab(1),
+                  isActive: activeTab === 1
+                },
+                {
+                  icon: 'ios-podium',
+                  title: 'Portfolio',
+                  onPress: () => setActiveTab(2),
+                  isActive: activeTab === 2
+                },
+                {
+                  icon: 'ios-people',
+                  title: 'Products',
+                  onPress: () => setActiveTab(3),
+                  isActive: activeTab === 3
+                },
+                {
+                  icon: 'ios-people',
+                  title: 'Friends',
+                  onPress: () => setActiveTab(4),
+                  isActive: activeTab === 4
+                }
+              ]}
+            />
+          </View>
+        </SafeAreaView>
+      </ThemeProvider>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  safeView: {
+    flex: 1,
+  },
+});
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { ActivityIndicator, FlatList, Text, View, StyleSheet, Image, ScrollView, TextInput, StatusBar} from 'react-native';
+import { ActivityIndicator, FlatList, Text, View, StyleSheet, Image, ScrollView, TextInput, StatusBar } from 'react-native';
 
 // or any pure javascript modules available in npm
-import { Card, Button, Title, Paragraph, Avatar, IconButton, Appbar } from 'react-native-paper';
+import { Card, Title, Paragraph, IconButton, Appbar } from 'react-native-paper';
+import { Button, Collection, Icon, Avatar, withTheme, Body } from 'react-native-ios-kit';
 
 export default function HomeScreen() {
 
@@ -18,58 +19,20 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <ScrollView>
-      <Appbar.Header></Appbar.Header>
-      <Card>
-        <Card.Title 
-          title="2019-12-11"
-          subtitle="Today"
-          left={(props) => <Avatar.Text {...props} label="XD" onPress={() => {}} />}
-          right={(props) => <IconButton {...props} icon="https://reactnative.dev/img/tiny_logo.png" onPress={() => {}} />}
-        />
-      </Card>
-
-      
-      <FlatList
-        data={data}
-        keyExtractor={({ id }, index) => id}
-        renderItem={({ item }) => (
-          <Card>
-        <Card.Title
-          title="AAPL"
-          subtitle="NASDAQ"
-          // left={(props) => <Avatar.Icon {...props} icon="folder" />}
-          // right={(props) => <IconButton {...props} icon="https://reactnative.dev/img/tiny_logo.png" onPress={() => {}} />}
-        />
-        <Card.Content>
-          <Paragraph>
-            3year_return: 11.37
-          </Paragraph>
-          <Paragraph>
-            standard_deviation: 20.12
-          </Paragraph>
-          <Paragraph>
-            dividend: 0.22
-          </Paragraph>
-          <Paragraph>
-            asset_class: Equity Developed Market
-          </Paragraph>
-        </Card.Content>
-        <Card.Content>
-          <Paragraph>
-            20 Friends Holding
-          </Paragraph>
-          <Paragraph>
-            1 Friends Following
-          </Paragraph>
-        </Card.Content>
-        <Card.Actions>
-          <Button>Buy</Button>
-        </Card.Actions>
-      </Card>
-        )}
-      />
-      <Appbar.Header></Appbar.Header>
-    </ScrollView>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text>ScrollView child component.</Text>
+      </ScrollView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: '1000',
+    backgroundColor: '#ecf0f1',
+  }
+});
